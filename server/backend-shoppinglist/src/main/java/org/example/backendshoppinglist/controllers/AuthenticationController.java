c2ab94e16ae64332a668143585b4e0e5e9d7bb67
mposiel <114227935+mposiel@users.noreply.github.com>
2024-05-14 17:07:32 +0200

    Added register and login mappings.

--- server/backend-shoppinglist/src/main/java/org/example/backendshoppinglist/controllers/AuthenticationController.java
+++ server/backend-shoppinglist/src/main/java/org/example/backendshoppinglist/controllers/AuthenticationController.java
@@ -1,5 +1,44 @@
 package org.example.backendshoppinglist.controllers;
 
+import org.example.backendshoppinglist.dtos.LoginUserDto;
+import org.example.backendshoppinglist.dtos.RegisterUserDto;
+import org.example.backendshoppinglist.entities.User;
+import org.example.backendshoppinglist.responses.LoginResponse;
+import org.example.backendshoppinglist.services.AuthenticationService;
+import org.example.backendshoppinglist.services.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/auth")
+@Controller
 public class AuthenticationController {
+    private AuthenticationService authenticationService;
+
+    private JwtService jwtService;
+
+    public AuthenticationController(AuthenticationService authenticationService, JwtService jwtService) {
+        this.authenticationService = authenticationService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody RegisterUserDto registerUserDto) {
+        User user = authenticationService.signup(registerUserDto);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+        String token = jwtService.generateToken(authenticatedUser);
+        LoginResponse loginResponse = new LoginResponse(
+                token,
+                jwtService.getExpirationTime()
+        );
+        return ResponseEntity.ok(loginResponse);
+    }
+
 }
---
+++ server/backend-shoppinglist/src/main/java/org/example/backendshoppinglist/responses/LoginResponse.java
@@ -0,0 +1,27 @@
+package org.example.backendshoppinglist.responses;
+
+public class LoginResponse {
+    public String token;
+    public long expiresIn;
+
+    public LoginResponse(String token, long expiresIn) {
+        this.token = token;
+        this.expiresIn = expiresIn;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
